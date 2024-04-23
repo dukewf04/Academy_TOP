28da0f7c5512408517f028e61ba39d77bb0874a8
Duke04 <zverwf@gmail.com>
2024-04-24 00:47:41 +0500

    ДЗ 24.04.24 Паттерн Абстрактная фабрика

--- Домашние задания/Классы/10. Паттерн Singleton 20.04.2024.py
+++ Домашние задания/Классы/10. Паттерн Singleton 20.04.2024.py
@@ -1,37 +1,128 @@
 # Task 1 (Singleton)
-class DataBase:
-    """Интерфейс подключения к базе данных"""
-    _instance = None
+# class DataBase:
+#     """Интерфейс подключения к базе данных"""
+#     _instance = None
+#
+#     def __new__(cls, *args, **kwargs):
+#         if cls._instance is None:
+#             cls._instance = super(DataBase, cls).__new__(cls)
+#         return DataBase._instance
+#
+#     def __init__(self, db_name, db_user, db_password, db_host, db_port):
+#         self.db_name = db_name
+#         self.db_user = db_user
+#         self.db_password = db_password
+#         self.db_host = db_host
+#         self.db_port = db_port
+#
+#     def connect(self):
+#         print(f"Подключение к базе данных {self.db_name} прошло успешно!")
+#
+# db1 = DataBase(
+#     db_name="HomeDB",
+#     db_user="Postgres",
+#     db_password="Postgres",
+#     db_host="localhost",
+#     db_port="5432",
+# )
+#
+# db2 = DataBase(
+#     db_name="WorkDB",
+#     db_user="root",
+#     db_password="herasebe",
+#     db_host="localhost",
+#     db_port="9999",
+# )
+#
+# print(f"db1: {db1.__dict__}\ndb2: {db2.__dict__}")
 
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(DataBase, cls).__new__(cls)
-        return DataBase._instance
 
-    def __init__(self, db_name, db_user, db_password, db_host, db_port):
-        self.db_name = db_name
-        self.db_user = db_user
-        self.db_password = db_password
-        self.db_host = db_host
-        self.db_port = db_port
+# Task 2 (Абстрактная фабрика)
+# import random
+# class Factory:
+#     def __init__(self, class_factory = None):
+#         self.class_factory = class_factory
+#
+#     def show(self):
+#         show_factory = self.class_factory()
+#         print("name:", show_factory, "\nprice:", show_factory.price(), "\n")
+#
+# class DSA:
+#     def price(self):
+#         return 111000
+#     def __str__(self):
+#         return "DSA"
+#
+# class STL:
+#     def price(self):
+#         return 15000
+#     def __str__(self):
+#         return "STL"
+#
+# class SDE:
+#     def price(self):
+#         return 8000
+#     def __str__(self):
+#         return "SDE"
+#
+# def random_factory():
+#     return random.choice([SDE, STL, DSA])()
+# factory = Factory(random_factory)
+#
+# for i in range(5):
+#     factory.show()
+# sde = SDE()
+# stl = STL()
+# dsa = DSA()
 
-    def connect(self):
-        print(f"Подключение к базе данных {self.db_name} прошло успешно!")
 
-db1 = DataBase(
-    db_name="HomeDB",
-    db_user="Postgres",
-    db_password="Postgres",
-    db_host="localhost",
-    db_port="5432",
-)
 
-db2 = DataBase(
-    db_name="WorkDB",
-    db_user="root",
-    db_password="herasebe",
-    db_host="localhost",
-    db_port="9999",
-)
-
-print(f"db1: {db1.__dict__}\ndb2: {db2.__dict__}")
+# Task 3 (Логирование операций)
+# import datetime as dt
+# class Loggin:
+#     """Логирование действий пользователя"""
+#     _instance = None
+#
+#     def __new__(cls, *args, **kwargs):
+#         if cls._instance is None:
+#             Loggin._instance = super(Loggin, cls).__new__(cls)
+#         return Loggin._instance
+#
+#     def __init__(self, path: str = "file"):
+#         self.log_fle = "log.txt"
+#         if path not in ("file", "screen"):
+#             raise ValueError("Введено не корректное значение!")
+#         self.path = path
+#         self.count = 0
+#
+#     def log(self, data):
+#         self.count += 1
+#         loggin_data = f"[{self.count}] {dt.datetime.now().strftime('%d-%m-Y %H:%M')}. {data}\n"
+#         if self.path == "file":
+#             with open(self.log_fle, "a", encoding="utf-8") as f:
+#                 f.write(loggin_data)
+#         elif self.path == "screen":
+#             print(loggin_data)
+#
+#
+# loggin = Loggin(path=input("Куда будут логироваться действия (file - в файл, screen - на экран)> "))
+#
+# numbers = list(input("Введите числа через пробел> ").strip().split())
+# loggin.log("Получен список чисел типа str")
+#
+# file = input("Введите путь к файлу, для сохранения данных> ")
+# loggin.log("Получен путь к файлу, для сохранения данных")
+#
+# numbers = list(map(int, numbers))
+# loggin.log("Преобразование списка str в список int")
+#
+# maxVal, minVal = max(numbers), min(numbers)
+# loggin.log("Найдены максимум и минимум чисел")
+#
+# with open(file, "w", encoding="utf-8") as f:
+#     data = f"Список чисел: {numbers}\nМаксимум: {maxVal}\nМинимум: {minVal}"
+#     f.write(data)
+#     loggin.log("Данные сохранены в файл")
+#
+#     print(data)
+#
--- Паттерны проектирования/Практика/Pasta.py
+++ Паттерны проектирования/Практика/Pasta.py
@@ -1,4 +1,5 @@
 import random
+import tabulate as tab
 
 # Task 2 (Создание 3 разных видов паст)
 # class PastaFactory:
@@ -49,78 +50,115 @@ import random
 
 
 # Пицца
-class PizzaFactory:
-    def __init__(self, class_factory = None):
-        self.factory = class_factory()
-    def show(self):
-        self.factory.get_pizza()
-
-class PizzaHome:
-    def get_pizza(self):
-        return "Пицца Домашняя\n\t- Вес: 1400 гр\n\t- Цена: 1900 р\n\t- Состав: Томатный соус, моцарелла, колбаса всех видов"
-
-class PizzaFourChees:
-    def get_pizza(self):
-        return "Пицца 4 сыра\n\t- Вес: 1200 гр\n\t- Цена: 1300 р\n\t- Состав: Томатный соус, моцарелла, пармезан, сыр горгонзола, артишоки и орегано"
-
-class PizzaKrudo:
-    def get_pizza(self):
-        return "Пицца Крудо\n\t- Вес: 700 гр\n\t- Цена: 800 р\n\t- Состав: Томатный соус, моцарелла и пармская ветчина"
-
-
-# Суши
-class SushiFactory:
-    def __init__(self, class_factory = None):
-        self.factory = class_factory()
-    def show(self):
-        self.factory.get_sushi()
-
-class SushiDomino:
-    def get_sushi(self):
-        return "Суши Домино\n\t- Вес: 800 гр\n\t- Цена: 1500 р\n\t- Состав: рис, форель"
-
-class SushiUramaki:
-    def get_sushi(self):
-        return "Суши Урамаки\n\t- Вес: 1000 гр\n\t- Цена: 2500 р\n\t- Состав: рис, угорь"
-
-class SushiGunkanMaki:
-    def get_sushi(self):
-        return "Суши Гункан-маки\n\t- Вес: 700 гр\n\t- Цена: 1400 р\n\t- Состав: салат, икра"
-
-
-
-# Роллы
-class RollFactory:
-    def __init__(self, class_factory = None):
-        self.factory = class_factory()
-    def show(self):
-        self.factory.get_roll()
-
-class RollBig:
-    def get_roll(self):
-        return "Роллы Big \n\t- Вес: 700 гр\n\t- Цена: 3000 р\n\t- Состав: рис, рыба"
-
-class RollMedium:
-    def get_shusi(self):
-        return "Роллы Medium\n\t- Вес: 1200 гр\n\t- Цена: 980 р\n\t- Состав: рис, молюски"
-
-class RollSmall:
-    def get_shusi(self):
-        return "Роллы Small\n\t- Вес: 1400 гр\n\t- Цена: 1900 р\n\t- Состав: рис, сыр, огурцы, колбаса"
-
-
-# Билдер
-class PZMilano:
-    def __init__(self):
-        self.sushi = None
-        self.pizza = None
-        self.roll = None
-
-class PZMilano_Builder:
-    def __init__(self):
-        self.pzmilano = PZMilano()
-        self.pzmilano.sushi = SushiFactory()
-        self.pzmilano.pizza = PizzaFactory()
-        self.pzmilano.roll = RollFactory()
-    def set_sushi(self):
-        self.pzmilano.sushi
+# class PizzaFactory:
+#     def __init__(self, class_factory = None):
+#         self.factory = class_factory()
+#     def show(self):
+#         return self.factory.get_pizza()
+#
+# class PizzaHome:
+#     def get_pizza(self):
+#         return "Пицца Домашняя\n\t- Вес: 1400 гр\n\t- Цена: 1900 р\n\t- Состав: Томатный соус, моцарелла, колбаса всех видов"
+#
+# class PizzaFourChees:
+#     def get_pizza(self):
+#         return "Пицца 4 сыра\n\t- Вес: 1200 гр\n\t- Цена: 1300 р\n\t- Состав: Томатный соус, моцарелла, пармезан, сыр горгонзола, артишоки и орегано"
+#
+# class PizzaKrudo:
+#     def get_pizza(self):
+#         return "Пицца Крудо\n\t- Вес: 700 гр\n\t- Цена: 800 р\n\t- Состав: Томатный соус, моцарелла и пармская ветчина"
+#
+#
+# # Суши
+# class SushiFactory:
+#     def __init__(self, class_factory = None):
+#         self.factory = class_factory()
+#     def show(self):
+#         return self.factory.get_sushi()
+#
+# class SushiDomino:
+#     def get_sushi(self):
+#         return "Суши Домино\n\t- Вес: 800 гр\n\t- Цена: 1500 р\n\t- Состав: рис, форель"
+#
+# class SushiUramaki:
+#     def get_sushi(self):
+#         return "Суши Урамаки\n\t- Вес: 1000 гр\n\t- Цена: 2500 р\n\t- Состав: рис, угорь"
+#
+# class SushiGunkanMaki:
+#     def get_sushi(self):
+#         return "Суши Гункан-маки\n\t- Вес: 700 гр\n\t- Цена: 1400 р\n\t- Состав: салат, икра"
+#
+#
+#
+# # Роллы
+# class RollFactory:
+#     def __init__(self, class_factory = None):
+#         self.factory = class_factory()
+#     def show(self):
+#         return self.factory.get_roll()
+#
+# class RollBig:
+#     def get_roll(self):
+#         return "Роллы Big \n\t- Вес: 700 гр\n\t- Цена: 3000 р\n\t- Состав: рис, рыба"
+#
+# class RollMedium:
+#     def get_roll(self):
+#         return "Роллы Medium\n\t- Вес: 1200 гр\n\t- Цена: 980 р\n\t- Состав: рис, молюски"
+#
+# class RollSmall:
+#     def get_roll(self):
+#         return "Роллы Small\n\t- Вес: 1400 гр\n\t- Цена: 1900 р\n\t- Состав: рис, сыр, огурцы, колбаса"
+#
+#
+# # Билдер
+# class PZMilano:
+#     def __init__(self):
+#         self.sushi = None
+#         self.pizza = None
+#         self.roll = None
+#
+#     def __str__(self):
+#         return f"Ваш заказ:\n {self.sushi if self.sushi else ''},\n" \
+#                f"{self.pizza if self.pizza else ''},\n" \
+#                f"{self.roll if self.roll else ''}\n"
+#
+# class PZMilano_Builder:
+#     def __init__(self):
+#         self.pzmilano = PZMilano()
+#         self.choise = None
+#
+#     def show_menu(self):
+#         print("********* МЕНЮ НА СЕГОДНЯ ********* ")
+#         menu = [
+#             ["Суши", "Пицца", "Роллы"],
+#             ["1. Домино", "4. Домашняя", "7. Big"],
+#             ["2. Урамаки", "5. 4 сыра", "8. Medium"],
+#             ["3. Гункан-маки", "6. Крудо", "9. Small"]
+#         ]
+#         print(tab.tabulate((menu)))
+#         self.choise = input("Ваш заказ (введите номер через пробел)?> ").split()
+#
+#         return self
+#
+#     def cooking(self):
+#         for el in self.choise:
+#             if el in ['1', '2', '3']:
+#                 self.pzmilano.sushi = SushiFactory(
+#                     [SushiDomino, SushiUramaki, SushiGunkanMaki][int(el)-1]
+#                 ).show()
+#             elif el in ['4', '5', '6']:
+#                 self.pzmilano.pizza = PizzaFactory(
+#                     [PizzaHome, PizzaFourChees, PizzaKrudo][int(el)-4]
+#                 ).show()
+#             elif el in ['7', '8', '9']:
+#                 self.pzmilano.roll = RollFactory(
+#                     [RollBig, RollMedium, RollSmall][int(el)-7]
+#                 ).show()
+#
+#         return self.pzmilano
+#
+#
+# pzm_builder = PZMilano_Builder()
+# pzm = pzm_builder.show_menu().cooking()
+#
+# print(pzm)
