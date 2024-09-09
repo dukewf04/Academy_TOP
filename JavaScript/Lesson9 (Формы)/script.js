784d733150f8d4db21ff46524ffb8f78c991cd73
Evgeny <zverwf@gmail.com>
2024-09-09 20:40:16 +0500

    JS 09.09.2024, формы

--- JavaScript/Lesson9 (Формы)/script.js
+++ JavaScript/Lesson9 (Формы)/script.js
@@ -1,39 +1,75 @@
-// Регулярные выражения
-// \d - любая цифра, \s - пробельный символ, \w - большая или маленькая буква
-// . - любой символ, \D - не цифра, \S - не пробел, \W - не символ
-// \x - не цифра и не буква
-// [0-9], [a-z], [а-я], [A-Z], [А-Я] - должны находиться
-// [^1-3] - не должны находиться, ставим степень
-// ? - 0 или 1 раз
-// +(плюс) - 1 и более вхождений
+//Регулярные выражения
+// \d - любая цифра, \s - пробельный символ, \w - большая или малькая буква, .(точка) - любой символ, \D - не цифра, \S -не пробел, \W- не символ, \x - не цифра(цифра, буква)
+// [0-9], [a-z],[a-я], [А-Я],[A-Z]
+//[^1-3] - не должны находится, ставим степень
+// ? - 0 или 1 раз,
+// +(плюс) - 1 и более раз,
+// * - n символов
 // {2} - ровно 2 повтора, {2,7} - от 2 до 7 повторов
-// Например: "\d{7}" - семь цифр подряд. "s+" - один или более пробельных символов
-// Для отображения спец. символов: "/$/"
-let templateName = /^[A-Z][a-z]*$/;
-let templatePhone = /^\+\d{12}$/;
-let templateLogin = /^[A-Z][a-z]*$/;
-const checkForm = (e) => {
-  let nameText = document.getElementById("name").value;
-  let loginText = document.getElementById("login").value;
-  let passwordText = document.getElementById("password").value;
-  let phoneText = document.getElementById("ph").value;
-  console.log(templateName.exec(nameText));
-  if (!templateName.exec(nameText)) {
-    alert("Неверное имя");
-    return false;
-  }
-  if (!templateLogin.exec(loginText)) {
-    alert("Неверный логин");
-    return false;
-  }
-  if (!passwordText.length < 6) {
-    alert("Неверный пароль");
-    return false;
-  }
-  if (!templatePhone.exec(phoneText)) {
-    alert("Неверный номер телефона");
-    return false;
-  }
+// Например: "\d{7}" - семь цифр подрят, "\s+" - один или более пробельный символ
+// для отображения спец. символа: "/$/"
+//var templateName = new RegExp("/^[a-zA-Z]+*$/");
+var templatePhone = /^\+\d{12}$/;
+var templateLogin = /^[A-Z][a-z]*$/;
+function checkForm() {
+  //обработка имени пользователя
+  var nameText = document.getElementById("name").value;
+  var loginText = document.getElementById("login").value;
+  var passwordText = document.getElementById("password").value;
+  var phoneText = document.getElementById("ph").value;
+  // if (!templateName.exec(nameText)) {
+  //   alert("Неверное имя");
+  //   return false;
+  // }
+  // //обработка логина
+  // if (!templateLogin.exec(loginText)) {
+  //   alert("Неверный логин");
+  //   return false;
+  // }
+  // //обработка пароля
+  // if (!passwordText.lenght < 6) {
+  //   alert("Неверный пароль");
+  //   return false;
+  // }
+  // if (!templatePhone.exec(phoneText)) {
+  //   alert("Неверный номер телефона");
+  //   return false;
+  // }
+
+  // Создание формы регистрации на основе cookie
+  var expDate = new Date;
+  expDate.setTime(expDate.getTime() + 60*1000)
+  document.cookie = `registered=${nameText};expires=${expDate.toGMTString()};path=/`
 
   return true;
+}
+
+const checkReg = () => {
+  let x = document.cookie;
+  let s = x.split(";");
+  let cookieObject = {};
+  let c;
+  for (var i = 0; i < s.length; i++) {
+    c = s[i].split("=");
+    cookieObject[c[0]] = c[1];
+  }
+
+  if ('registered' in cookieObject) {
+    regDiv.innerHTML = `Hello ${cookieObject['registered']}`
+  }
+
+  // Добавление cookie-файлов в проект
+  // document.cookie =
+  //   "registered='User'; expires='Mon', 09 sep 2024 18:38:40 GMT; path=/";
+
+  // window.out.innerHTML = document.cookie;
+  // registered - это имя пользователя
+  // expires - срок действия куки-файлов
+  // path - Облать действия куки
+  // domain - имя сайта (сервера), отправившего данную страницу
+  // secure - протокол шифрования
+  // document.cookie - получить все параметры для создания и заполнения
+  // До 300 cookie файлов
+  // от 1 домена(сайта) можно хранить до 20 значений.
+
 };
