56a8a118cfaa9eb056df20002846541ae3525854
Evgeny <zverwf@gmail.com>
2024-09-30 21:09:03 +0500

    DJANGO 30.09.2024

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 # Формы представлений для страниц приложения
 def index(request):
     return HttpResponse("<b>Hello User!</b>")
+
+
+# Действие с опросами
+def detail(request, question_id):
+    return HttpResponse("You're looking at question", question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
