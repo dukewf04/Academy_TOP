5cbc06d6983b9bdd6453716ce5eb8c81b845ed7e
Evgeny <zverwf@gmail.com>
2024-10-07 20:58:36 +0500

    DJANGO 07.10.24

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,35 +1,60 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Формы представлений для страниц приложения
-def index(request):
-    try:
-        latest_question_list = Question.objects.order_by("-pub_date")[:5]
-        templeate = loader.get_template('polls/index.html')
-        context = {
-            "latest_question_list": latest_question_list,
-            "request": request,
-        }
-    except:
-        raise Http404("Нет вопросов")
-    #output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(templeate.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_questyset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+    # try:
+    #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    #     templeate = loader.get_template("polls/index.html")
+    #     context = {
+    #         "latest_question_list": latest_question_list,
+    #         "request": request,
+    #     }
+    # except:
+    #     raise Http404("Нет вопросов")
+    # # output = ", ".join([q.question_text for q in latest_question_list])
+    # return HttpResponse(templeate.render(context, request))
 
 
 # Действие с опросами
-def detail(request, question_id):
-    return HttpResponse("You're looking at question", question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    #response = "You're looking at the results of question %s."
+    #return HttpResponse(response % question_id)
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError):
+        return render(
+            request, "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Вы не сделали выбор!",
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponse(reverse("polls:results", args=(question.id,)))
--- pythonProject/HAIL.GIDRA!/py.py
+++ pythonProject/HAIL.GIDRA!/py.py
@@ -1,31 +1,82 @@
-from random import randint as  rand
+# from random import randint as rand
+#
+# spis = [i for i in range(10)]
+# spis = [rand(-30, 30) for i in range(10)]
+# cauntNega = 0
+# cauntEven = 0
+# cauntOdd = 0
+# min1 = min(spis)
+# max1 = max(spis)
+# res3 = 1
+# pros = 1
+# print(spis)
+# for j in range(len(spis)):
+#     if spis[j] < 0:
+#       cauntNega += 1
+#     if spis[j] % 2 == 0:
+#       cauntEven += 1
+#     elif spis[j] != 0:
+#         cauntOdd += 1
+# for b in range(len(spis)):
+#     if b % 3 == 0:
+#         res3 *= spis[b]
+# # for d in range(min1, max1):
+# #         pros *= spis[d]
+# print(f'Отрицательных чисел: {cauntNega}')
+# print(f'Четные: {cauntEven}')
+# print(f'Нечетные: {cauntOdd}')
+# print(f'Произведение с индексами кратные трем: {res3}')
+# print(f'Произведение чисел между min и max: {pros}')
+#4 числа,
+# devoperation4(value1, value2, value4, value4):
+#2
 
-list = [i for i in range(10)]
-print(list)
-list = [rand(-20, 20) for i in range(10)]
-print(list)
+# def operation4(value1, value2, value3, value4 ):
+#  print(f'Max: {max(value1, value2, value3, value4)}')
+#  print(f'Min: {min(value1, value2, value3, value4)}')
+#  print(f'Sum: {(value1 + value2 + value3 + value4)}')
+#  print(f'MediumA: {(value1 + value2 + value3 + value4) / 4}')
+#
+# value1 = int(input('Введите второе число: '))
+#
+# value2 = int(input('Введите первое число: '))
+#
+# value3 = int(input('Введите третье число: '))
+#
+# value4 = int(input('Введите четвертое число: '))
+#
+# operation4(value1, value2, value3, value4)
 
-caunt = 0
-caunt2 = 0
-caunt3 = 0
-res = 1
-res1 = 1
-min1 = min(list)
-max1 = max(list)
-for j in range(len(list)):
-    if list[j] < 0:
-        caunt += list[j]
-    if list[j] % 2 == 0:
-        caunt2 += list[j]
-    elif list[j] % 2 != 0:
-        caunt3 += list[j]
-    if j % 3 == 0:
-         res *= list[j]
-    for b in range(min1, max1):
-          res1 *= list[j]
+#3
 
-print(caunt)
-print(caunt2)
-print(caunt3)
-print(res)
-print(res1)
+# def line(string, k):
+#     return string * k
+# a = input('Enter sim: ')
+# b = int(input('How much duplication: '))
+# print(f'{line(a, b)}\n\t')
+
+# def line(string, k):
+#     s = ' '
+#     for i in range(k):
+#         s += string + "\n"
+#     return s
+# a = input('Enter sim: ')
+# b = int(input('How much duplication: '))
+# print(line(a, b))
+
+# def sum(par1, par2):
+#     if par1 > par2:
+#         par1,par2 = par2,par1
+#     summ = 0
+#     for i in range(par1, par2):
+#        summ += i
+#     return summ
+# par1 = int(input(''))
+# par2 = int(input(''))
+# print(sum(par1, par2))
+
+def lep(digit):
+        if int(digit[0]) +  int(digit[1]) +  int(digit[2]) == int(digit[-1]) +  int(digit[-2]) +  int(digit[-3]):
+         return digit
+        print(f'd {lep(digit)}')
+digit = (input())
